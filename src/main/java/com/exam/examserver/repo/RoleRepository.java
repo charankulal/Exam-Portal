cf573bbc8c140fa174e8c77c6b6b9977814e9128
Charan <charankulal0241@gmail.com>
2024-01-16 22:22:35 +0530

    Updated file structure

--- src/main/java/com/exam/examserver/repo/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.exam.examserver.repo;
-
-import com.exam.examserver.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,Long> {
-}
