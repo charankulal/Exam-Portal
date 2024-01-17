27ff115f39186a32b0d3ccb5ee91fd9d99db1148
Charan <charankulal0241@gmail.com>
2024-01-17 23:41:55 +0530

    Created formSubmit function to retrieve data from input fields.

--- examfront/src/app/app.component.ts
+++ examfront/src/app/app.component.ts
@@ -5,6 +5,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {NavbarComponent} from './components/navbar/navbar.component'
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +17,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     NavbarComponent,
     MatInputModule,
     MatFormFieldModule,
+    FormsModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- examfront/src/app/pages/signup/signup.component.ts
+++ examfront/src/app/pages/signup/signup.component.ts
@@ -1,19 +1,35 @@
 import { Component } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
   imports: [
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    FormsModule
   ],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
 export class SignupComponent {
-
+  public user = {
+    username: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+  };
+  formSubmit() {
+    console.log(this.user);
+    if (this.user.username == '' || this.user.username == null) {
+      alert('Username is required');
+      return;
+    }
+  }
 }
