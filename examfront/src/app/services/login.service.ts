feea7a3b539727b4524305cab4a6293df16033c5
Charan <charankulal0241@gmail.com>
2024-01-22 13:57:39 +0530

    Added some utility functins to login service

--- examfront/src/app/services/login.service.ts
+++ examfront/src/app/services/login.service.ts
@@ -16,4 +16,70 @@ export class LoginService {
   {
     return this.http.post(`${baseUrl}/generate-token`,loginData)
   }
+
+  //login user : store the token in localstorage
+
+  public loginUser(token:any)
+  {
+    localStorage.setItem('token',token)
+    return true;
+  }
+
+  // TO check whether use is  loggedin or not
+  public isLoggedIn()
+  {
+    let tokenString= localStorage.getItem('token')
+    console.log("User logged in",tokenString)
+    if(tokenString== undefined || tokenString==''|| tokenString==null)
+    {
+      return false
+    }
+    else{
+      return true
+    }
+  }
+
+  // To logout: remove token from localstorage
+  public logout()
+  {
+    localStorage.removeItem('token')
+    localStorage.removeItem('user')
+    return true
+  }
+
+  //to get token
+  public getToken()
+  {
+    return localStorage.getItem('token')
+  }
+
+  //set user detail
+  public setUser(user:any)
+  {
+    localStorage.setItem('user',JSON.stringify(user))
+
+  }
+
+  //get user detail
+  public getUser()
+  {
+    let userString=localStorage.getItem('user')
+    console.log(userString)
+    if(userString!=null)
+    {
+      return JSON.parse(userString)
+    }
+    else
+    {
+      this.logout()
+      return null
+    }
+  }
+
+  //get user role or authority
+  public getUserRole()
+  {
+    let user=this.getUser()
+    return user.authorities[0].authority
+  }
 }
